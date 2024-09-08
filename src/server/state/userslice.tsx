5d836df9f16aac3bbb55997ee12820569c173e92
shirashtub <shirashtub@gmail.com>
2024-09-08 14:01:46 +0300

    user

--- src/server/state/userslice.tsx
+++ src/server/state/userslice.tsx
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import Cookies from 'js-cookie';
 import axios from "axios";
 
@@ -6,15 +6,39 @@ const id = Cookies.get('userId');
 const http = import.meta.env.VITE_HTTPCALL;
 const initialState = {
     status: id===undefined?"DETACHED":"CONNECTED",
-    user: id===undefined?{}:async () => (await (axios.get(http + `/user/auth/${id}`))).data
+    user: {
+        name: "אורח"
+    },
+    loading: true,
+    error: null
 }
 
 export const userSlice = createSlice({
     name: 'user',
     initialState,
-    reducers: {
-    }
+    reducers: { 
+    },
+    extraReducers: (builder) => {
+        builder
+          .addCase(fetchUser.pending, (state) => {
+            state.loading = true;
+            state.error = null;
+          })
+          .addCase(fetchUser.fulfilled, (state, action) => {
+            state.user = action.payload;
+            state.status = 'CONNECTED';
+            state.loading = false;
+          })
+      }
 });
 
 export const { } = userSlice.actions;
 export default userSlice.reducer;
+
+export const fetchUser = createAsyncThunk(
+    'user/fetchUser',
+    async () => {
+      const response = await axios.get(`${http}/user/auth/${id}`);
+      return response.data;
+    }
+  );
