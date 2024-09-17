f6ed79cdfefdf3a134fdb3603c955ea08cb4474f
ayalashraiber <ayalash44@gmail.com>
2024-09-17 15:56:49 +0300

    add link

--- src/components/businessmanagement/businessmanagement.tsx
+++ src/components/businessmanagement/businessmanagement.tsx
@@ -1,10 +1,12 @@
 import { GenericPage } from '../generic/genericpage'
 import { Home, TextSnippet } from "@mui/icons-material"
+import BusinessIcon from '@mui/icons-material/Business';
 import { HomePage } from './homepage'
 // import { About } from "./about"
 import { useAppSelector } from '../../server/state/hooks'
 import { useEffect } from 'react'
 import { About } from './about'
+import { Business } from '@mui/icons-material'
 
 export const BusinessManagement = () => {
 
@@ -19,6 +21,8 @@ export const BusinessManagement = () => {
         <GenericPage title={"business management"} actions={[
           {name: "עמוד ראשי", icon: <Home/>, href: "home", component: HomePage },
           {name: "אודות", icon: <TextSnippet/>, href: "about", component: About },
+          {name: "עסקים", icon: <BusinessIcon/>, href: "business", component: Business },
+
         ]} colors={colors}/>
     </>
   )
