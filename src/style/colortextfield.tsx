f28720e20ad313e7e060a564badfbd6191dd024d
shirashtub <shirashtub@gmail.com>
2024-11-03 17:35:39 +0200

    newcontact

--- src/style/colortextfield.tsx
+++ src/style/colortextfield.tsx
@@ -1,32 +1,3 @@
-// import { TextField, styled } from "@mui/material"
-
-// type ColorxTextFieldProps = {
-//     colorx?: string;
-//   };
-  
-// export const ColorxTextField = styled(TextField, {
-//     shouldForwardProp: (prop) => prop !== 'colorx',
-//   })<ColorxTextFieldProps>(({ colorx = 'black' }) => ({
-//     '& .MuiInput-underline': {
-//       borderBottom: `2px solid ${colorx}`, 
-//     },
-//     '& .MuiInput-underline:after': {
-//       borderBottom: `2px solid ${colorx}`,
-//     },
-//     '& .MuiInputLabel-root': {
-//       right: 0,
-//       left: 'unset', 
-//       textAlign: 'right', 
-//       color: colorx,
-//     },
-//     '& .MuiInputLabel-shrink': {
-//       color: `${colorx} !important`,
-//     },
-//     '& .MuiInputBase-input': {
-//       color: colorx,
-//     }
-// }));
-
 import { TextField, styled } from "@mui/material";
 
 type ColorxTextFieldProps = {
